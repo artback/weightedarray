ac455a968ee2be166d1d4e9b4114ca9110bddf39
Artback <artback@protonmail.com>
2020-06-05 20:50:04 +0200

    Implement the weighted random array function in go

--- weightedarray.go
+++ weightedarray.go
@@ -1,34 +1,32 @@
 package weightedarray
 
 import (
-	"fmt"
-	"github.com/gonum/floats"
-	"gonum.org/v1/gonum/stat/distuv"
 	"math/rand"
 	"sort"
 	"time"
 )
 
 func init() {
-	rand.Seed(time.Now().UnixNano())
+	rand.Seed(time.Now().Unix())
 }
-func getWeights(arr []struct{}, weightFn func(interface{}) float64) ([]float64, error) {
-	var weights []float64
-	for _, val := range *a {
-		weights = append(weights, weightFn(val))
-	}
-	if weights == nil || len(weights) == 0 {
-		return nil, fmt.Errorf("invalid probability weights: %v", weights)
-	}
-	return weights, nil
+
+type Value interface {
+	GetWeight() float64
 }
-func random(arr []struct{}, weightFn func(interface{}) float64) (interface{}, error) {
-	weights, err := getWeights(a,weightFn)
-	if err != nil {
-		return nil, err
+type Values []Value
+
+func (values Values) Random() interface{} {
+	sort.Slice(values, func(i, j int) bool {
+		return values[i].GetWeight() < values[j].GetWeight()
+	})
+	totals := make([]int, len(values))
+	runningTotal := 0
+	for i, v := range values {
+		runningTotal += int(v.GetWeight())
+		totals[i] = runningTotal
 	}
-	cdf := make([]float64, len(weights))
-	floats.CumSum(cdf, weights)
-	var val = distuv.UnitUniform.Rand() * cdf[len(cdf)-1]
-	return sort.Search(len(cdf), func(i int) bool { return cdf[i] > val }), nil
+	r := rand.Intn(runningTotal) + 1
+	i := sort.SearchInts(totals, r)
+	return values[i]
+
 }
